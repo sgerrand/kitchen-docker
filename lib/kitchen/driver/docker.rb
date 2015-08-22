91a6acdc29f4a2fea21a6d13236f0d01bea85194
Sasha Gerrand <github-kitchen-docker@sgerrand.com>
2015-08-22 09:43:17 +0100

    Support legacy Tempfile create method
    Ruby versions prior to 2.1.0 don't support `Tempfile.create`. Wrapping the
    tempfile creation process allows a conditional approach to be used.

--- lib/kitchen/driver/docker.rb
+++ lib/kitchen/driver/docker.rb
@@ -254,12 +254,20 @@ module Kitchen
         cmd << " --no-cache" unless config[:use_cache]
         dockerfile_contents = dockerfile
         build_context = config[:build_context] ? '.' : '-'
-        output = Tempfile.create('Dockerfile-kitchen-', Dir.pwd) do |file|
+        output = create_tempfile('Dockerfile-kitchen-', Dir.pwd) do |file|
           file.write(dockerfile_contents)
           file.close
           docker_command("#{cmd} -f #{file.path} #{build_context}", :input => dockerfile_contents)
         end
         parse_image_id(output)
+      end
+
+      def create_tempfile(basename, dir)
+        if Tempfile.respond_to?(:create)
+          Tempfile.create(basename, dir)
+        else
+          Tempfile.new(basename, dir)
+        end
       end
 
       def parse_container_id(output)
